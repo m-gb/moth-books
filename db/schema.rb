ee7b1684da5a9879313a8c20cc471f0cfd0e9056
maya-go <mayu666@gmail.com>
2017-03-14 22:34:49 +0100

    Add image filename column to books table.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170313224350) do
+ActiveRecord::Schema.define(version: 20170314212941) do
 
   create_table "books", force: :cascade do |t|
     t.string   "title"
@@ -19,8 +19,9 @@ ActiveRecord::Schema.define(version: 20170313224350) do
     t.integer  "category_id"
     t.integer  "year"
     t.string   "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+    t.string   "image_filename"
   end
 
   create_table "categories", force: :cascade do |t|
