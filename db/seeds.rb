a63f5ee343a00671d04f1b833b14d6f3f6d68571
maya-go <mayu666@gmail.com>
2017-03-16 00:15:35 +0100

    Finish book view

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,17 +13,17 @@ Category.create id: 2, name: "Science Fiction"
 Book.delete_all
 Book.create id: 1, title: "The Lord of the Rings Trilogy", author: "J.R.R. Tolkien",
             category_id: Category.find(1).id, year: 1954,
-            image_filename: "books/the_lord_of_the_rings_trilogy.gif",
-            description: "blablabla"
+            image_filename: "books/the_lord_of_the_rings_trilogy.png",
+            description: "blablabla", price: 10.99
 Book.create id: 2, title: "The Hobbit", author: "J.R.R. Tolkien",
             category_id: Category.find(1).id, year: 1937,
-            image_filename: "books/the_hobbit.jpg"
+            image_filename: "books/the_hobbit.png"
 Book.create id: 3, title: "The Silmarillion", author: "J.R.R. Tolkien",
             category_id: Category.find(1).id, year: 1977,
             image_filename: "books/the_silmarillion.png"
 Book.create id: 4, title: "Good Omens", author: "Terry Pratchett & Neil Gaiman",
             category_id: Category.find(1).id, year: 1990,
-            image_filename: "books/good_omens.jpg"
+            image_filename: "books/good_omens.png"
 Book.create id: 5, title: "Ender's Game", author: "Orson Scott Card",
             category_id: Category.find(2).id, year: 1985,
-            image_filename: "books/enders_game.jpg"
+            image_filename: "books/enders_game.png"
