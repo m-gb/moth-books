623835498a627fd2c21a50c9661ce8e63780ea60
maya-go <mayu666@gmail.com>
2017-03-20 00:40:28 +0100

    implemented devise

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,9 @@
 class BooksController < ApplicationController
 
+  def index
+
+  end
+
   def show
     @book = Book.find(params[:id])
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,6 @@
 class User < ApplicationRecord
-  before_save { email.downcase! }
-  validates :name,  presence: true, length: { maximum: 50}
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-  validates :email, presence: true, length: { maximum: 255 },
-                    format: { with: VALID_EMAIL_REGEX },
-                    uniqueness: { case_sensitive: false }
-  has_secure_password
-  VALID_PASSWORD_REGEX = /\A(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,64}\z/
-  validates :password, presence: true, length: { minimum: 8, maximum: 64 },
-                       format: { with: VALID_PASSWORD_REGEX }
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
 end
