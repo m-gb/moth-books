a4fb452d29394d23b14578e739c0adffeb49145d
maya-go <mayu666@gmail.com>
2017-05-10 00:53:56 +0200

    Finished carousel

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,21 @@
 class StaticPagesController < ApplicationController
   def home
-    @books = Book.all
+    sql_query = "SELECT book_id, COUNT(book_id) AS count FROM cart_items
+                 GROUP BY book_id
+                 ORDER BY count DESC
+                 LIMIT 5;"
+    query_hash = CartItem.connection.select_all(sql_query).to_hash
+    @book_ids = []
+    query_hash.each do |result|
+      @book_ids << result["book_id"] # returned from db as string
+    end
+    @books = []
+    @book_ids.each do |b_id|
+      @books << Book.find(b_id)
+    end
+
+    
+
   end
 
   def help
