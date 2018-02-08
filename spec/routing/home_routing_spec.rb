f01eeba9ecdba210b44b7af27fddb24d06a8311c
m-gb <maya.goldberg90@gmail.com>
2018-02-08 20:48:37 +0100

    finish routing spec

--- spec/routing/home_routing_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe "routing to static pages", type: :routing do
-  it "routes root to static_pages#home" do
-    expect(get: root_path).
-      to route_to(controller: "static_pages", action: "home")
-  end
-end
